1ee73f535c5f078f9d481bfd85a52d63e57d7533
Parameswari Bala <parameswaribala@gmail.com>
2024-10-07 10:12:42 +0530

    control statements selected

--- admin/admin.py
+++ admin/admin.py
@@ -1,1 +1,26 @@
+<<<<<<< HEAD
 #creating administrator functionalities
+=======
+#creating administrator functionalities
+
+let data=("admin1","admin2");
+
+#control statements
+import random;
+#generate 100 random numbers
+genData=0;
+for i in range(1,10):
+    genData=random.randint(1,500);
+    if (genData>100):
+      print(genData,end="\t");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+>>>>>>> 3d7049b (control statements added)
